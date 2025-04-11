788a9505009f7be4f751d241fb73f0515f9c3ff7
Nisar Ahmed Bhutto <159589661+nisarahmedbhutto@users.noreply.github.com>
2025-04-12 00:54:03 +0500

    secure_data.py

--- secure_data.py
+++ secure_data.py
@@ -31,7 +31,7 @@ def load_data():
     if os.path.exists(DATA_FILE):
         with open(DATA_FILE,"r") as f:
             return json.load(f)
-        return {}
+    return {}
     
 def save_data(data):
         with open(DATA_FILE,"w") as f:
